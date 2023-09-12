43a70d4f8bdfe0024c5d19bf25085db384157693
Andrii Mamchenko <atmamont@gmail.com>
2023-09-12 14:38:48 +0200

    Loading places triggers API request

--- adyen-places-ios/PlacesTests/RemotePlacesLoaderTests.swift
+++ adyen-places-ios/PlacesTests/RemotePlacesLoaderTests.swift
@@ -7,12 +7,47 @@
 
 import XCTest
 import AdyenNetworking
+@testable import Places
 
 class RemotePlacesLoader {
+    typealias LoadResult = (Result<[PlaceItem], Error>) -> Void
+    
     private let apiClient: APIClientProtocol
     
     init(apiClient: APIClientProtocol) {
         self.apiClient = apiClient
+    }
+    
+    func load(completion: @escaping LoadResult) {
+        apiClient.perform(SearchPlacesRequest()) { result in
+            switch result {
+            case let .success(response):
+                completion(.success(self.map(response.results)))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private func map(_ remotePlaces: [RemotePlaceItem]) -> [PlaceItem] {
+        remotePlaces.toModels()
+    }
+    
+    private struct Root: Decodable {
+        let results: [RemotePlaceItem]
+    }
+
+}
+
+private extension Array where Element == RemotePlaceItem {
+    func toModels() -> [PlaceItem] {
+        map {
+            PlaceItem(
+                latitude: $0.geocodes.main.latitude,
+                longitude: $0.geocodes.main.longitude,
+                name: $0.name,
+                description: "")
+        }
     }
 }
 
@@ -24,11 +59,23 @@ final class RemotePlacesLoaderTests: XCTestCase {
         XCTAssertEqual(client.performCallCount, 0, "Expected to not perform network request on init")
     }
     
+    func test_load_performsRequest() {
+        let client = APIClientSpy()
+        let sut = RemotePlacesLoader(apiClient: client)
+        
+        sut.load { _ in
+        }
+        
+        XCTAssertEqual(client.performCallCount, 1, "Expected to perform request on load call")
+    }
+    
 
     private class APIClientSpy: APIClientProtocol {
         var performCallCount = 0
+        var requestedPaths = [String]()
         
         func perform<R: Request>(_ request: R, completionHandler: @escaping CompletionHandler<R.ResponseType>) {
+            requestedPaths.append(request.path)
             performCallCount += 1
         }
     }
