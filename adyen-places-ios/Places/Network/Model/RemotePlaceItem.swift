647bf774df9bc7feeb35d9b6508167201de6380f
Andrii Mamchenko <atmamont@gmail.com>
2023-09-12 14:37:34 +0200

    Add basic parameters for RemotePlaceItem including geocodes so we can easily display any place on the map if needed

--- adyen-places-ios/Places/Network/Model/RemotePlaceItem.swift
+++ adyen-places-ios/Places/Network/Model/RemotePlaceItem.swift
@@ -7,5 +7,23 @@
 import Foundation
 
 internal struct RemotePlaceItem: Decodable {
+    let name: String
+    let geocodes: Geocodes
+    let location: [Location]
+    let distance: Int
+}
 
+extension RemotePlaceItem {
+    struct Location: Decodable {
+        let formatted_address: String
+    }
+    
+    struct Geocodes: Decodable {
+        let main: Geocode
+        let roof, drop_off, front_door: Geocode?
+    }
+    
+    struct Geocode: Decodable {
+        let latitude, longitude: Double
+    }
 }
