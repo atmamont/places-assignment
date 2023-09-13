be5dcf3b46c4d31a92abdc83e2b1e9bda70eaf51
Andrii Mamchenko <atmamont@gmail.com>
2023-09-13 14:00:14 +0200

    Prototype UI part to unblock quick end-to-end validations

--- adyen-places-ios/PlacesUIKit/ViewController.swift
+++ adyen-places-ios/PlacesUIKit/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import Places
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    // TODO: Inject from outside
+    var loader: PlacesLoader? = PlacesLoaderAssembly.foursquareLoader()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loader?.load(completion: { [weak self] result in
+            switch result {
+            case let .success(items):
+                self?.label.text = items.toString()
+            case let .failure(error):
+                self?.label.text = error.localizedDescription
+            }
+        })
     }
-
-
 }
 
+private extension Array where Element == PlaceItem {
+    func toString() -> String {
+        (self as NSArray).componentsJoined(by: ", ")
+    }
+}
