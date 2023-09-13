fb1ca46bec5760a53cb37445ea284a04185d2e45
Andrii Mamchenko <atmamont@gmail.com>
2023-09-13 16:14:17 +0200

    Add test suite for new `LocationController` implementation `CoreLocationController`. Check SUT does not request authorization on instantiation

--- adyen-places-ios/PlacesTests/CoreLocationControllerTests.swift
+++ adyen-places-ios/PlacesTests/CoreLocationControllerTests.swift
@@ -5,4 +5,45 @@
 //  Created by Andrei on 13/09/2023.
 //
 
-import Foundation
+import XCTest
+import Places
+import CoreLocation
+
+final class CoreLocationController {
+    var currentLocation: LocationController.Location?
+    
+    private let locationManager: CLLocationManager
+    
+    init(locationManager: CLLocationManager = CLLocationManager()) {
+        self.locationManager = locationManager
+    }
+
+//    func requestAuthorization() async -> LocationAuthorizationStatus {
+//        return .granted
+//    }
+//
+//    func startUpdating() {
+//
+//    }
+//
+//    func stopUpdating() {
+//
+//    }
+}
+
+class CoreLocationManagerTests: XCTestCase {
+    func test_init_doesNotRequestAuthorizationWhenInUse() {
+        let manager = CLLocationManagerSpy()
+        let _ = CoreLocationController(locationManager: manager)
+        
+        XCTAssertEqual(manager.requestWhenInUseAuthorizationCallCount, 0)
+    }
+    
+    private class CLLocationManagerSpy: CLLocationManager {
+        var requestWhenInUseAuthorizationCallCount = 0
+        
+        override func requestWhenInUseAuthorization() {
+            requestWhenInUseAuthorizationCallCount += 1
+        }
+    }
+}
