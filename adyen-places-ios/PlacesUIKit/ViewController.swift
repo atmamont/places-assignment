b41f1345f88c54ca2e30750b17ae8841dfcf39e6
Andrii Mamchenko <atmamont@gmail.com>
2023-09-13 20:35:15 +0200

    Continue with prototyping: add Map view, render loaded places on the map, focus map to the last loaded place

--- adyen-places-ios/PlacesUIKit/ViewController.swift
+++ adyen-places-ios/PlacesUIKit/ViewController.swift
@@ -8,29 +8,80 @@
 import UIKit
 import Places
 
+import MapKit
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var label: UILabel!
     
     // TODO: Inject from outside
     var loader: PlacesLoader? = PlacesLoaderAssembly.foursquareLoader()
-    
+    lazy var locationController: LocationController = {
+        let controller = CoreLocationController()
+        controller.locationUpdateHandler = self.handleLocationUpdate
+        return controller
+    }()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationController.requestAuthorization()
+        locationController.startUpdating()
         
         loader?.load(completion: { [weak self] result in
             switch result {
             case let .success(items):
-                self?.label.text = items.toString()
+                self?.renderAnnotations(items.toAnnotations())
             case let .failure(error):
                 self?.label.text = error.localizedDescription
             }
         })
+    }
+    
+    func handleLocationUpdate(location: LocationController.Location) {
+        print(location)
+        locationController.stopUpdating()
+    }
+
+    // MARK: - Map rendering
+    
+    private func renderAnnotations(_ annotations: [MKAnnotation]) {
+        mapView.addAnnotations(annotations)
+        
+        if let lastAnnotation = annotations.last {
+            focusMap(on: lastAnnotation)
+        }
+    }
+    
+    private func focusMap(on annotation: MKAnnotation) {
+        let region = MKCoordinateRegion(center: annotation.coordinate,
+                                        span: mapView.region.span)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+private class Venue: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(latitide: Double, longitude: Double, title: String?, subtitle: String? = nil) {
+        self.coordinate = .init(latitude: latitide, longitude: longitude)
+        self.title = title
+        self.subtitle = subtitle
     }
 }
 
 private extension Array where Element == PlaceItem {
     func toString() -> String {
         (self as NSArray).componentsJoined(by: ", ")
+    }
+    
+    func toAnnotations() -> [MKAnnotation] {
+        map { Venue(latitide: $0.latitude,
+                    longitude: $0.longitude,
+                    title: $0.name,
+                    subtitle: $0.address) }
     }
 }
