4520ac18a693e603a1c30204dcb62f4ecb963753
Andrii Mamchenko <atmamont@gmail.com>
2023-09-14 11:57:48 +0200

    Tune view controller behaviour so it fetches places after 1) user location is known 2) user adjusts the search radius

--- adyen-places-ios/PlacesUIKit/ViewController.swift
+++ adyen-places-ios/PlacesUIKit/ViewController.swift
@@ -11,15 +11,25 @@ import MapKit
 import Places
 
 class ViewController: UIViewController {
-
-    @IBOutlet private weak var mapView: MKMapView!
-    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var mapView: MKMapView! {
+        didSet {
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    private var lastKnownUserLocation: Location?
+    private var radius: Int = 1000 {
+        didSet {
+            focusAndScaleMap(on: lastKnownUserLocation)
+            fetchPlaces()
+        }
+    }
     
     // TODO: Inject from outside
     var loader: PlacesLoader? = PlacesLoaderAssembly.foursquareLoader()
     lazy var locationController: LocationController = {
         let controller = CoreLocationController()
-        controller.locationUpdateHandler = self.handleSingleLocationUpdate
+        controller.locationUpdateHandler = self.handleFirstLocationUpdate
         return controller
     }()
         
@@ -28,39 +38,53 @@ class ViewController: UIViewController {
         
         locationController.requestAuthorization()
         locationController.startUpdating()
-        
-        loader?.load(completion: { [weak self] result in
+    }
+    
+    // MARK - Private
+    
+    private func fetchPlaces() {
+        loader?.load(location: lastKnownUserLocation,
+                     radius: radius)
+        { [weak self] result in
+            guard let self else { return }
+            
             switch result {
             case let .success(items):
-                self?.renderAnnotations(items.toAnnotations())
+                self.renderAnnotations(items.toAnnotations())
             case let .failure(error):
                 AlertHelper.showAlert(
                     title: R.networkErrorAlertTitle,
                     message: error.localizedDescription,
                     from: self)
             }
-        })
+        }
     }
     
-    func handleSingleLocationUpdate(location: LocationController.Location) {
+    private func handleFirstLocationUpdate(location: Location) {
         locationController.stopUpdating()
+        lastKnownUserLocation = location
+
+        fetchPlaces()
     }
 
     // MARK: - Map
     
     private func renderAnnotations(_ annotations: [MKAnnotation]) {
         mapView.addAnnotations(annotations)
-        
-        if let lastAnnotation = annotations.last {
-            focusMap(on: lastAnnotation)
-        }
+        let focusPoint = lastKnownUserLocation
+        focusAndScaleMap(on: focusPoint)
     }
     
-    private func focusMap(on annotation: MKAnnotation) {
-        let region = MKCoordinateRegion(center: annotation.coordinate,
-                                        span: mapView.region.span)
+    private func focusAndScaleMap(on location: Location?) {
+        guard let location else { return }
+        let region = MKCoordinateRegion(
+            center: location.toCLCoordinate(),
+            latitudinalMeters: CLLocationDistance(radius),
+            longitudinalMeters: CLLocationDistance(radius))
         mapView.setRegion(region, animated: true)
     }
+    
+    // MARK: - Actions
     
     @IBAction private func onSliderValueChange(_ sender: UISlider) {
         radius = Int(sender.value)
@@ -85,5 +109,19 @@ private extension Array where Element == PlaceItem {
                     longitude: $0.longitude,
                     title: $0.name,
                     subtitle: $0.address) }
+    }
+}
+
+private extension Location {
+    func toCLCoordinate() -> CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude,
+                               longitude: longitude)
+    }
+}
+
+extension MKAnnotation {
+    func toLocation() -> Location {
+        Location(latitude: coordinate.latitude,
+                 longitude: coordinate.longitude)
     }
 }
