21421ddcc6914abc28d074049c6762aeed7ec5bd
Andrii Mamchenko <atmamont@gmail.com>
2023-09-14 10:50:27 +0200

    Provide public initializer for Location struct

--- adyen-places-ios/Places/PlacesFeature/Location.swift
+++ adyen-places-ios/Places/PlacesFeature/Location.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 public struct Location {
-    let latitude, longitude: Double
+    public let latitude, longitude: Double
+    
+    public init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
 
 extension Location {
