2849ab29a7835b68aa9aa15c0050b97c37166b02
Andrii Mamchenko <atmamont@gmail.com>
2023-09-15 09:30:19 +0200

    Create a spy for location controller, test if location authorization request is triggered on viewDidLoad and not triggered on initialisation

--- adyen-places-ios/PlacesUIKitTests/PlacesViewControllerTests.swift
+++ adyen-places-ios/PlacesUIKitTests/PlacesViewControllerTests.swift
@@ -13,19 +13,59 @@ final class PlacesViewControllerTests: XCTestCase {
     
     func test_load_doesNotTriggerFetch() {
         let loader = RemotePlacesLoaderSpy()
-        let sut = PlacesViewController(loader: loader)
+        let locationController = LocationControllerSpy()
+        let sut = PlacesViewController(loader: loader,
+                                       locationController: locationController)
         
         sut.loadView()
         
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_init_doesNotTriggerLocationRequest() {
+        let loader = RemotePlacesLoaderSpy()
+        let locationController = LocationControllerSpy()
+        let sut = PlacesViewController(loader: loader,
+                                       locationController: locationController)
+        
+        XCTAssertEqual(locationController.requestAuthorizationCallCount, 0)
+    }
+
+    func test_load_triggersLocationRequest() {
+        let loader = RemotePlacesLoaderSpy()
+        let locationController = LocationControllerSpy()
+        let sut = PlacesViewController(loader: loader,
+                                       locationController: locationController)
+
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(locationController.requestAuthorizationCallCount, 1)
+    }
+
     private class RemotePlacesLoaderSpy: PlacesLoader {
         var loadCallCount = 0
         func load(location: Places.Location?, radius: Int?, completion: @escaping (LoadResult) -> Void) {
             loadCallCount += 1
         }
+    }
+    
+    private class LocationControllerSpy: LocationController {
+        var requestAuthorizationCallCount = 0
+        var startUpdatingCallCount = 0
+        var stopUpdatingCallCount = 0
+
+        var locationUpdateHandler: ((Places.Location) -> Void)?
+
+        func requestAuthorization() {
+            requestAuthorizationCallCount += 1
+        }
         
+        func startUpdating() {
+            startUpdatingCallCount += 1
+        }
         
+        func stopUpdating() {
+            stopUpdatingCallCount += 1
+        }
     }
 }
